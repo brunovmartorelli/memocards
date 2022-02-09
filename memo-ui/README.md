36e620cbf6fa4744bc4609d0d0613fd9f0f0f3a9
brunovmartorelli <brunovmartorelli@gmail.com>
2022-02-09 17:14:53 -0300

    Botao criar deck na home

--- memo-ui/README.md
+++ memo-ui/README.md
@@ -1,5 +1,7 @@
 # TODO
-
+- [ ] Criar botao para criar deck na home
+- [ ] Redirecionar para a pagina de edicao ao criar o deck
+- [ ] Criar cartas dentro dos decks
 - [ ] Página de estudo dentro dos decks
 - [ ] Criar variaveis de ambiente
 - [ ] Tratamentos de erro na camada de service
--- memo-ui/pages/index.js
+++ memo-ui/pages/index.js
@@ -2,12 +2,27 @@ import DeckList from "../components/deckList/deckList"
 import { list } from "./../services/deck.service"
 import { withTheme } from '@emotion/react'
 import MemoHeading from "./../components/memoheading/memoheading"
+import { Flex, Box, Button } from "rebass"
+import theme from "../theme"
+import Link from "next/link"
 
 const Index = ({ decks }) => {
     return (
         <>
-            <MemoHeading title='MEMOCARDS'/>
+            <MemoHeading title='MEMOCARDS' />
             <DeckList decks={decks} />
+            <Flex justifyContent='center' mx={-2} mb={3}>
+                <Box px={2}>
+                    <Link href='/createDeck'>
+                        <Button
+                            style={{ cursor: 'pointer' }}
+                            backgroundColor={theme.discordblue}
+                            fontFamily='Roboto'>
+                            Criar Deck
+                        </Button>
+                    </Link>
+                </Box>
+            </Flex>
         </>
     )
 }
@@ -20,5 +35,7 @@ export async function getStaticProps() {
         }
     }
 }
+
+
 
 export default withTheme(Index)
